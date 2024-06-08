8700d5ac4533079130223fe1f2ce9da4f6d19356
Himanshu12866 <manshu010m@gmail.com>
2024-06-08 17:25:02 +0530

    Added

---
+++ script.js
@@ -0,0 +1,64 @@
+
+ScrollReveal({
+    reset: false,
+    distance: "60px",
+    duration: 1500,
+    delay: 100,
+});
+ScrollReveal().reveal(" .section-1-heading , .section-3-heading , .section-7-text , .section-8-img , .section-10", {
+    origin: "bottom",
+});
+ScrollReveal({
+    reset: false,
+    distance: "80px",
+    duration: 1000,
+    delay: 100,
+});
+ScrollReveal().reveal(" .section-1-img , .play-btn , .section-9-box , .footer-text", {
+    origin: "top",
+});
+ScrollReveal({
+    reset: false,
+    distance: "80px",
+    duration: 1000,
+    delay: 100,
+});
+ScrollReveal().reveal(" .section-2-img, .costumers", {
+    origin: "left",
+});
+ScrollReveal({
+    reset: false,
+    distance: "60px",
+    duration: 1000,
+    delay: 100,
+});
+ScrollReveal().reveal(" .section-2-text, .section-6-text , .overlay-container", {
+    origin: "right",
+});
+ScrollReveal({
+    reset: false,
+    distance: "100px",
+    duration: 1000,
+    delay: 100,
+});
+ScrollReveal().reveal(" .section-box-1 , .section-box-2", {
+    origin: "right",
+});
+ScrollReveal({
+    reset: false,
+    distance: "80px",
+    duration: 1000,
+    delay: 200,
+});
+ScrollReveal().reveal(" .data-box-1, .section-4-img", {
+    origin: "right",
+});
+ScrollReveal({
+    reset: false,
+    distance: "80px",
+    duration: 1000,
+    delay: 200,
+});
+ScrollReveal().reveal(" .section-4-text , .section-9-text", {
+    origin: "left",
+});
